c0b4b0a19f1846a863544f40bc0b32579b4c5147
James Ortiz <jameso2@alumni.stanford.edu>
2019-03-15 14:51:33 -0400

    Added storyboard elements to display collection of quotes and quote details

---
+++ Inspira/QuoteDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QuoteDetailViewController.swift
+//  Inspira
+//
+//  Created by James Ortiz on 3/15/19.
+//  Copyright © 2019 James Ortiz. All rights reserved.
+//
+
+import UIKit
+
+class QuoteDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
